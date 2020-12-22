04302fc9b103a2c7c10c5151da719930c3abcbe7
arpita207 <sayhitoarpita@gmail.com>
2020-12-22 08:35:21 +0530

    added Substring comparisons

--- Java/Strings/Java Substring Comparisons/Solution.java
+++ Java/Strings/Java Substring Comparisons/Solution.java
@@ -27,8 +27,7 @@ public class Solution {
     }
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         String s = scan.next();
         int k = scan.nextInt();
